ac2ed24f79359a119520a508af37be9db1ffd610
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-03-31 15:45:43 -0300

    feat: Newsletter section

---
+++ app/components/Newsletter/Newsletter.tsx
@@ -0,0 +1,23 @@
+export default function Newsletter() {
+  return (
+    <div className="pt-20 pb-20 bg">
+      <div className="text-center">
+        <h1 className="text-[28px] sm:text-[38px] md:text-[50px] text-black font-bold">
+          Join for hot offers
+        </h1>
+
+        <p className="text-black text-opacity-70 w-[85%] md:w-[60%] mx-auto">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis dolorem neque saepe eum aspernatur doloribus iusto maiores excepturi atque voluptates esse, itaque voluptatem quos. Nobis nisi quae exercitationem repellat!
+        </p>
+
+        <div className="flex justify-center items-center space-x-2 mt-8 md:mt-12">
+          <input type="email" placeholder="Email adress" className="px-5 py-4 bg-gray-300 w-[50%] outline-none rounded-lg placeholder:text-black" />
+
+          <button className="px-5 py-4 md:px-8 bg-[#F2A900] hover:bg-green-700 transition-all duration-300 rounded-md text-white font-bold">
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
